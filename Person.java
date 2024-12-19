cbf135a7deeebf99f301782ce66a4496f83bb2fa
AshokBohara <156417004+AshokBohara@users.noreply.github.com>
2024-12-19 20:33:40 +0545

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,23 +1,27 @@
-// Person.java
-public class Person {
+// example of class in java 
+public class Person 
+{
     // Fields
     String name;
     int age;
 
     // Constructor
-    public Person(String name, int age) {
+    public Person(String name, int age) 
+    {
         this.name = name;
         this.age = age;
     }
 
     // Method to display details
-    public void displayDetails() {
+    public void displayDetails() 
+    {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
 
     // Main method
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // Creating an object of the Person class
         Person person = new Person("Ashok", 20);
         person.displayDetails(); // Calling the method
