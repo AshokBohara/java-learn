41fcfb5998272f2fe086df5d4cb3993d90751748
Ashok Bohara <boharaashok32@gmail.com>
2025-01-04 21:11:53 +0545

    lab2 Qn. 2 in java

---
+++ Lab/lab2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class lab2 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        // Create an ArrayList to store the numbers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt());
+        }
+        // Sort the ArrayList in ascending order
+        Collections.sort(numbers);
+
+        System.out.println("Numbers in ascending order:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
