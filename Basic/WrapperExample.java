3ab9675b1b97e7970e7b350d600724b2bb3321a9
Ashok Bohara <boharaashok32@gmail.com>
2025-01-20 07:19:59 +0545

    example of wrapper class in java

---
+++ Basic/WrapperExample.java
@@ -0,0 +1,15 @@
+public class WrapperExample
+ {
+    public static void main(String[] args)
+     {
+        Integer num = 10;
+
+        int value = num;
+
+        String str = Integer.toString(num);
+        int parsedValue = Integer.parseInt("25");
+
+        System.out.println("Value: " + value);
+        System.out.println("Parsed Value: " + parsedValue);
+    }
+}
