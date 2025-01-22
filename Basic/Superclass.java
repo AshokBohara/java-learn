3dacca03b18a9754cd5f7088c8d935e1a819e9b6
Ashok Bohara <boharaashok32@gmail.com>
2025-01-22 20:10:04 +0545

    example of super keybord inn java

---
+++ Basic/Superclass.java
@@ -0,0 +1,26 @@
+class Superclass
+ {
+    void message()
+     {
+        System.out.println("Welcome");
+    }
+}
+
+class Students extends Superclass {
+    void message()
+     {
+        System.out.println("Welcome to Java");
+    }
+
+    void display()
+     {
+        message();
+        super.message();
+    }
+
+    public static void main(String[] args)
+     {
+        Students s = new Students();
+        s.display();
+    }
+}
