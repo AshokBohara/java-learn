9d9273cb36a7aa5fdfd912e3fc147ae95485c1c7
Ashok Bohara <boharaashok32@gmail.com>
2025-02-24 21:28:35 +0545

    check if input string is palindrome or not

---
+++ Basic/palindrome.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Palindrome
+ {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.getData();
+        d.checkPalindrome();
+    }
+}
+
+class Data {
+    private String str;
+
+    public void getData() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a string: ");
+            str = sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void checkPalindrome() {
+        if (str == null) {
+            System.out.println("No valid input received.");
+            return;
+        }
+        
+        String reversed = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
