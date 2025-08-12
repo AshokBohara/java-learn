6b9e1247157c6a67d53b12c337a7d1f20d98a534
Ashok Bohara <boharaashok32@gmail.com>
2025-08-12 21:49:27 +0545

    modified

--- Basic/Demooo.java
+++ Basic/Demooo.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
-
 interface interfacee
 {
     void getUserValue();
     void displayUserDetail();
 }
-
-class Demooo implements interfacee {
-
+class Demooo implements interfacee
+{
     String name, address;
     int age;
-
-    public void getUserValue() {
+    public void getUserValue()
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name:");
         name = sc.nextLine();
@@ -20,16 +18,17 @@ class Demooo implements interfacee {
         System.out.println("Enter your age:");
         age = sc.nextInt();
     }
-
-    public void displayUserDetail() {
+    public void displayUserDetail()
+    {
         System.out.println("Name: " + name);
         System.out.println("Address: " + address);
         System.out.println("Age: " + age);
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Demooo user = new Demooo();
         user.getUserValue();
         user.displayUserDetail();
+        System.out.println("Thank you for providing your details.");
     }
 }
